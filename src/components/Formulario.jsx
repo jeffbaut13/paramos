03f342e22ff156fd0422475e39ad62a21fa50def
jeffbaut13 <jeferssonabv@gmail.com>
2024-03-26 08:53:27 -0500

    ajustar navbar

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -3,7 +3,9 @@ import axios from "axios";
 
 function ContactForm() {
   const [name, setName] = useState("");
+  const [ciudad, setCiudad] = useState("");
   const [whatsapp, setWhatsApp] = useState("");
+  const [edad, setEdad] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
   const [formStatus, setFormStatus] = useState("");
@@ -13,20 +15,19 @@ function ContactForm() {
 
   const fileUrls = [
     "/descargar/descargable.mp4",
-    "/descargar/IMAGEN-DESCARGABLE.jpg"
+    "/descargar/IMAGEN-DESCARGABLE.jpg",
   ];
 
   const downloadFiles = () => {
-    fileUrls.forEach(url => {
-      const link = document.createElement('a');
+    fileUrls.forEach((url) => {
+      const link = document.createElement("a");
       link.href = url;
-      link.setAttribute('download', ''); 
+      link.setAttribute("download", "");
       document.body.appendChild(link);
       link.click();
       document.body.removeChild(link);
     });
   };
-
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -34,7 +35,9 @@ function ContactForm() {
 
     const formData = {
       name,
+      ciudad,
       whatsapp,
+      edad,
       email,
       message,
     };
@@ -48,23 +51,27 @@ function ContactForm() {
       // Limpia el formulario después del envío exitoso o maneja la respuesta como desees
       downloadFiles();
       setName("");
+      setCiudad("");
       setWhatsApp("");
+      setEdad("");
       setEmail("");
       setMessage("");
     } catch (error) {
       setFormStatus("Hubo un error al enviar el formulario.");
-
     } finally {
-        setIsLoading(false); // Finaliza la carga después de recibir la respuesta
-      }
+      setIsLoading(false); // Finaliza la carga después de recibir la respuesta
+    }
   };
 
   return (
-    <div className="contact-form   w-[80%] max-lg:pb-6">
-      <form onSubmit={handleSubmit} className=" flex-col">
+    <div className="contact-form h-full w-[80%] max-lg:pb-6">
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col justify-evenly h-full py-12"
+      >
         <div className="form-group">
           <input
-            className="bg-transparent border-b-[1px] lg:py-2 lg:w-[60%] xs:w-full max-lg:text-xs cajasFormMobile placeholder-white text-white"
+            className="bg-transparent border-b-[1px]  lg:w-[60%] xs:w-full max-lg:text-xs cajasFormMobile placeholder-white text-white"
             type="text"
             placeholder="NOMBRE"
             value={name}
@@ -73,16 +80,34 @@ function ContactForm() {
         </div>
         <div className="form-group ">
           <input
-            className="bg-transparent border-b-[1px] lg:py-2 my-8 margenFormMobile max-lg:text-xs lg:w-[60%] xs:w-full cajasFormMobile placeholder-white text-white"
+            className="bg-transparent border-b-[1px] margenFormMobile max-lg:text-xs lg:w-[60%] xs:w-full cajasFormMobile placeholder-white text-white"
+            type={"text"}
+            placeholder="CIUDAD"
+            value={ciudad}
+            onChange={(e) => setCiudad(e.target.value)}
+          />
+        </div>
+        <div className="form-group ">
+          <input
+            className="bg-transparent border-b-[1px] margenFormMobile max-lg:text-xs lg:w-[60%] xs:w-full cajasFormMobile placeholder-white text-white"
             type={inputType}
             placeholder="NÚMERO DE WHATSAPP"
             value={whatsapp}
             onChange={(e) => setWhatsApp(e.target.value)}
           />
         </div>
+        <div className="form-group ">
+          <input
+            className="bg-transparent border-b-[1px] margenFormMobile max-lg:text-xs lg:w-[60%] xs:w-full cajasFormMobile placeholder-white text-white"
+            type={inputType}
+            placeholder="EDAD"
+            value={edad}
+            onChange={(e) => setEdad(e.target.value)}
+          />
+        </div>
         <div className="form-group">
           <input
-            className="bg-transparent border-b-[1px] lg:py-2 lg:w-[60%] max-lg:text-xs xs:w-full cajasFormMobile placeholder-white text-white"
+            className="bg-transparent border-b-[1px]  lg:w-[60%] max-lg:text-xs xs:w-full cajasFormMobile placeholder-white text-white"
             type="email"
             placeholder="CORREO ELECTRÓNICO"
             value={email}
@@ -92,17 +117,23 @@ function ContactForm() {
         <div className="form-group">
           <textarea
             value={message}
-            className="placeTextAreaContacto-placeholder bg-[#ffffff1c] max-lg:text-xs rounded-lg xl:pb-14 xs:pb-6 lg:w-[60%] xs:w-full my-8 cajasFormMobile text-white"
-            placeholder="Máximo 1800 caracteres"
+            className="placeTextAreaContacto-placeholder bg-[#ffffff1c] max-lg:text-xs rounded-lg xl:pb-14 xs:pb-6 lg:w-[60%] xs:w-ful cajasFormMobile text-white"
+            placeholder="Cuéntanos por qué quieres ser un Guardián"
             onChange={(e) => setMessage(e.target.value)}
             maxLength="1800"
           />
         </div>
         <div className="form-group">
           {isLoading ? (
-            <div className="text-start  text-white mb-4 bg-[#ffffff1c] lg:w-[60%] xs:w-full text-xs">Enviando formulario...</div>
+            <div className="text-start  text-white mb-4 bg-[#ffffff1c] lg:w-[60%] xs:w-full text-xs">
+              Enviando formulario...
+            </div>
           ) : (
-            formStatus && <div className="text-start text-white mb-4 bg-[#ffffff1c] lg:w-[60%] xs:w-full text-xs">{formStatus}</div>
+            formStatus && (
+              <div className="text-start text-white mb-4 bg-[#ffffff1c] lg:w-[60%] xs:w-full text-xs">
+                {formStatus}
+              </div>
+            )
           )}
         </div>
         <div className="lg:w-[60%] xs:w-full flex justify-center">
